918cc998c4067b1182a68a798844b45fa0b7a2cd
fxmarty <9808326+fxmarty@users.noreply.github.com>
2022-01-25 10:05:30 +0100

    added a script to extract some images from many takes

---
+++ extract_images.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import sys
+
+from shutil import copyfile
+
+parser = argparse.ArgumentParser(description='Parser')
+
+parser.add_argument('--folder',
+                    help='Folder where .jpg images are stored',
+                    type=str,
+                    required=True)
+
+args = parser.parse_args()
+
+# normpath to remove the last '/' if written, which must not be there for split
+parent_path, folder = os.path.split(os.path.normpath(args.folder))
+
+output_folder = parent_path
+output_folder = os.path.join(output_folder, folder + '_extract')
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in sorted(os.listdir(args.folder)):
+    if filename.endswith('.jpg'):
+        parts = filename.split('-')
+
+        n_horizontal_shot = parts[3][2:]  # e.g. hI15 so remove hI
+        n_horizontal_shot = int(n_horizontal_shot)
+
+        n_vertical_shot = parts[1][2:]  # e.g. vI2 so remove vI
+        n_vertical_shot = int(n_vertical_shot)
+
+        if n_horizontal_shot % 4 == 0 and n_vertical_shot % 2 == 0:
+            print(f'Copying {filename}...')
+            copyfile(os.path.join(args.folder, filename),
+                     os.path.join(output_folder, filename))
